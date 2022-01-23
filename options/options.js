ea84d059b11c4013492bbcf90a748f4a8cff44ba
Crowdbotics <team@crowdbotics.com>
2022-01-23 07:16:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "selftapepro_33083",
+  url: "https://selftapepro_33083.botics.co",
+  api: "https://selftapepro_33083.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
